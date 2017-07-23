46cde646ce4e5c47d63f16945c64881c3d97cc47
Marco Ochse <t3chn0m4g3@gmail.com>
2017-07-23 21:30:54 +0000

    listbot: account for CIDR notations

--- logstash/dist/gen_iprep_map.sh
+++ logstash/dist/gen_iprep_map.sh
@@ -33,7 +33,25 @@ local myHOST=$(echo $2 | cut -d "/" -f3)
   fi
 }
 
+# Only match lines with CIDR addresses, unzip if necessary
+# Duplicates will be eliminated for the final translation map!
+fuMATCHCIDR () {
+local myFILE=$1
+
+  if [ -f $myFILE ];
+    then
+      myZIP=$(file $myFILE | grep -c "Zip")
+      if [ "$myZIP" == "1" ]
+        then
+          unzip -p $myFILE | grep -o -P "\b(?:\d{1,3}\.){3}\d{1,3}/\d{1,2}\b" | xargs -I '{}' prips '{}'
+        else
+          grep -o -P "\b(?:\d{1,3}\.){3}\d{1,3}/\d{1,2}\b" $myFILE | xargs -I '{}' prips '{}'
+      fi
+  fi
+}
+
 # Only match lines with IPv4 addresses, unzip if necessary
+# Duplicates will be eliminated for the final translation map!
 fuMATCHIP () {
 local myFILE=$1
 
@@ -57,7 +75,8 @@ local myTMPFILE="$3.tmp"
 local myYAMLFILE="$3.raw"
 
   fuCURL $myTMPFILE $myURL
-  fuMATCHIP $myTMPFILE | awk '{ print "\""$1"\": \"" "'"$myTAG"'" "\"" }' > $myYAMLFILE
+  fuMATCHCIDR $myTMPFILE | awk '{ print "\""$1"\": \"" "'"$myTAG"'" "\"" }' > $myYAMLFILE
+  fuMATCHIP $myTMPFILE | awk '{ print "\""$1"\": \"" "'"$myTAG"'" "\"" }' >> $myYAMLFILE
   mySIZE=$(wc -l < $myYAMLFILE)
     if [ "$mySIZE" != "0" ]
       then
@@ -65,7 +84,6 @@ local myYAMLFILE="$3.raw"
       else
         echo "[ Control output ] [$myRED EMPTY FILE $myWHITE]"
     fi
-
 }
 
 # Download reputation lists
@@ -104,6 +122,6 @@ fuDOWNLOAD "https://myip.ms/files/blacklist/general/full_blacklist_database.zip"
 # Generate logstash translation map for ip reputation lookup
 echo -n "[ Building translation map ] "
 cat *.raw > $myIPREPMAP.tmp
+# Remove duplicates
 sort -u $myIPREPMAP.tmp > $myIPREPMAP
 echo "[$myGREEN DONE $myWHITE]"
-
